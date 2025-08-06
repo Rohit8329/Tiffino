a42168337899ba4dc8dcdb6fb81d95cc8ee8735c
Rohit 🚀 <78404433+Rohit8329@users.noreply.github.com>
2025-08-06 17:00:21 +0530

    Create environment.ts

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://3.110.64.224:8890',
+  appName: '',
+  enableDebug: true
+};
+
+
+// export const environment = {
+//   production: false,
+//   apiBaseUrl: 'http://3.110.64.224:8890', // for login and others
+//   subscriptionApiBaseUrl: 'http://3.110.64.224:8940', // ✅ NEW entry for subscription services
+
+//   appName: '',
+//   enableDebug: true
+// };
+ 
